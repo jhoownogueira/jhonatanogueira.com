e006b4c85b3ac2448c05e26cefddf6fb6d0cf33a
Jhonata Nogueira <jhoownogueira@outlook.com>
2022-08-23 17:32:10 -0300

    Start Projeto em ReactJs

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { GlobalStyle } from './Styles/global';
+
+export function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </div>
+  );
+}
